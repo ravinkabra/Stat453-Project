a318f6ca03b2df8a6bdd526fb6839be8af810086
S-tanley <2962786177@qq.com>
2025-10-15 15:36:06 +0000

    add inference code

---
+++ src/model/old_pt_m2a_transformer_with_att/inference.py
@@ -0,0 +1,177 @@
+from src.model.old_pt_m2a_transformer_with_att.model import OldPtM2ATransformerWithAttention, EOS_TOKEN, PAD_TOKEN
+from src._utils.preprocess_midi2pt_dataset import preprocess_midi, DURATION_TEMPLATES
+import torch
+import pretty_midi
+import os
+import argparse
+from typing import Literal
+import numpy as np
+# import pdb
+import time
+
+def decode_output(outputs, save_path, tempo=120.0, prompt=True, single=False):
+    midi = pretty_midi.PrettyMIDI(initial_tempo=tempo)
+    time_step_length = 60.0 / tempo / 4
+    if not isinstance(outputs, tuple):
+        outputs = (outputs,)
+    for output in outputs:
+        instrument_map: dict[Literal[0, 1], pretty_midi.Instrument] = {}
+        for time_step, data in enumerate(output):
+            content = data.squeeze(0)
+            time_step = time_step if single else time_step // 2
+            start_time = time_step * time_step_length
+            for i in range(0, len(content), 2):
+                program = int(content[i].item())
+                if program == EOS_TOKEN:
+                    break
+                if i + 1 >= len(content):
+                    print("Incomplete note @", time_step, i)
+                    break
+                pitch_duration = int(content[i + 1].item())
+                pitch_duration = pitch_duration - 2
+                pitch = pitch_duration % 128
+                duration = pitch_duration // 128
+                if program != 0 and program != 1:
+                    print("Invalid program:", program, "@", time_step, i)
+                    break
+                if pitch < 0 or pitch >= 128:
+                    print("Invalid pitch:", pitch, "@", time_step, i)
+                    break
+                if duration < 0 or duration >= len(DURATION_TEMPLATES):
+                    print("Invalid duration:", duration, "@", time_step, i)
+                    break
+                end_time = DURATION_TEMPLATES[duration] * time_step_length + start_time
+                if program not in instrument_map:
+                    if program == 0:
+                        inst = pretty_midi.Instrument(program=24, name="Guitar")
+                    else:  # program == 1
+                        inst = pretty_midi.Instrument(program=0, name="Piano")
+                    instrument_map[program] = inst
+                    midi.instruments.append(inst)
+
+                inst = instrument_map[program]
+                inst.notes.append(pretty_midi.Note(velocity=100, pitch=pitch, start=start_time, end=end_time))
+
+    os.makedirs(os.path.dirname(save_path), exist_ok=True)
+    midi.write(save_path)
+
+# change dim, and do the preprocess
+# a list of subseq: [num_logical_ticks, max_polyphony*3] 
+# ->
+# [1, num_logical_ticks, max_polyphony*2]
+def decompress(model, byte_arr_mel, byte_arr_acc, device):
+    x = torch.tensor(byte_arr_mel).unsqueeze(0)
+    x = x.to(device)  # Move to the specified device
+    y = torch.tensor(byte_arr_acc).unsqueeze(0)
+    y = y.to(device)  # Move to the specified device
+    return model.preprocess(x, pitch_shift=torch.zeros(1, dtype=torch.int8).to(device), y=y)
+
+
+def continuation(model, midi_path, prompt_length=100, generation_length=400, temperature=1.0, n_samples=1, gt_mel=True, device='cuda'):
+    if os.path.isfile(midi_path):
+        pass
+    else:
+        print(f"Error: {midi_path} is not a valid file.")
+    byte_arr_mel = preprocess_midi(midi_path, 4)
+    byte_arr_acc = preprocess_midi(midi_path.replace("mel", "acc"), 4)
+
+    if byte_arr_mel is None:
+        print(f"Error: preprocess_midi returned None for mel file: {midi_path}")
+        return  # Skip this MIDI file
+    if byte_arr_acc is None:
+        print(f"Error: preprocess_midi returned None for acc file: {midi_path.replace('mel', 'acc')}")
+        return  # Skip this MIDI file
+
+    x_mel, x_acc = decompress(model, byte_arr_mel[0], byte_arr_acc[0], device=device)
+
+    if prompt_length == 0:
+        B, S, L = x_mel.shape
+        valid = (x_mel != EOS_TOKEN) & (x_mel != PAD_TOKEN)  # [B, S, L]，True 表示该 token 有效
+        frame_has = valid.any(dim=2)  # [B, S]，True 表示这一帧有非 (EOS 或 PAD) 的 token
+        idx = torch.arange(S, device=x_mel.device).unsqueeze(0).expand(B, S)  # [B, S]
+        idx_masked = torch.where(frame_has, idx, torch.full_like(idx, S))  # [B, S]
+        first_timestep = idx_masked.min(dim=1).values
+    else:
+        first_timestep = 1
+    if not gt_mel:
+        decode_output(
+            [x_mel[:, i, :] for i in range(x_mel.shape[1])],
+            f"temp/{model.save_name}/{os.path.basename(midi_path)}_originalmelody.mid",
+            single=True,
+            tempo=90.0,
+        )
+    x_mel_gt = x_mel.clone()
+    x_mel_gt = x_mel_gt[:, first_timestep - 1 + prompt_length :]
+    x_mel = x_mel[:, :prompt_length]
+    x_acc = x_acc[:, :prompt_length]
+    batch_size, seq_len, subseq_len = x_mel.shape  # 10*384*8
+    stacked = torch.stack([x_acc, x_mel], dim=2)
+    x = stacked.view(batch_size, seq_len * 2, subseq_len)
+
+    if prompt_length != 0:
+        decode_output(
+            [x[:, i, :] for i in range(x.shape[1])], f"temp/{model.save_name}/{os.path.basename(midi_path)}_promptlen{prompt_length}.mid", tempo=90.0
+        )
+
+    with torch.no_grad():
+        x = x.repeat(n_samples, 1, 1)
+        x_mel_gt = x_mel_gt.repeat(n_samples, 1, 1)
+
+        start_time = time.time()
+        if prompt_length == 0:
+            output = model.global_sampling_from_scratch(x_mel_gt, temperature=temperature, max_seq_len=generation_length)
+        else:
+            output = model.global_sampling(x, x_mel_gt=x_mel_gt if gt_mel else None, temperature=temperature, max_seq_len=generation_length)
+        end_time = time.time()
+        print(f"Generation time: {end_time - start_time:.2f} seconds")
+
+    for i in range(n_samples):
+        output_i = [output[j][i : i + 1, :] for j in range(len(output))]
+        with open(f"tensor_output_original_inference_wo_gt_{i}.txt", "w") as f:
+            f.write(str(output_i))
+        decode_output(output_i, f"temp/{model.save_name}/prompt{prompt_length}/{os.path.basename(midi_path)}_temp{temperature}_{i}.mid", tempo=90.0)
+
+
+if __name__ == "__main__":
+    # seed = 42
+    # np.random.seed(seed)
+    # torch.manual_seed(seed)
+    # torch.cuda.manual_seed_all(seed)
+    start_pre_time = time.time()
+
+    parser = argparse.ArgumentParser(description="process midi folder(s) into usable tensors for the task")
+
+    parser.add_argument("--model_path", type=str, default="../shared_models/ModelBaseline/cp_transformer_909+ac+1k7_trackemb_interleavepos_v0.2_large_batch_40_schedule.epoch=00.val_loss=0.90296.ckpt", help="path to model checkpoint")
+    parser.add_argument("--prompt_len", type=int, default=75, help="length of prompt")
+    parser.add_argument("--n_samples", type=int, default=1, help="number of samples")
+    parser.add_argument("--temperature", type=float, default=1.0, help="temperature")
+    parser.add_argument("--model_size", type=str, default='0.12B', help="model size")
+
+    args = parser.parse_args()
+
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    print(f"Using device: {device}")
+
+    model_path = args.model_path
+    model_path = "/home/ubuntu/stanleyz/shared_models/BaselineDropout/0.3/epoch=743-step=150288.ckpt"
+
+    model = OldPtM2ATransformerWithAttention.load_from_checkpoint(checkpoint_path=model_path, map_location=device)
+    model.save_name = os.path.basename(model_path)
+    model.to(device)  # Move model to GPU
+    model.eval()
+
+    midi_file_path_set = ['/home/ubuntu/stanleyz/StreamMUSE/inference_benchmark/inputs/aria_unique_skyline_top2_subset_5/mel', '/home/ubuntu/stanleyz/StreamMUSE/input/mel', '/home/ubuntu/stanleyz/StreamMUSE/inference_benchmark/inputs/test_set/mel']
+
+    end_pre_time = time.time()
+    print(f"Model loading time (before the inference): {end_pre_time - start_pre_time:.2f} seconds")
+
+    for midi_file_path in midi_file_path_set:
+        for midi in os.listdir(midi_file_path):
+            if midi.endswith('mid'):
+                midi = os.path.join(midi_file_path, midi)
+                start_true_generation_time = time.time()
+                continuation(model, midi, temperature=args.temperature, generation_length=400, n_samples=args.n_samples, prompt_length=args.prompt_len, gt_mel=False, device=device)
+                end_true_generation_time = time.time()
+                print(f"Total time for generating {midi}: {end_true_generation_time - start_true_generation_time:.2f} seconds")
+    # midi = '/home/ubuntu/ugrip/stanleyz/StreamMUSE/input/mel/001.mid'
+    # continuation(model, midi, temperature=args.temperature, generation_length=100, n_samples=args.n_samples, prompt_length=args.prompt_len, gt_mel=False)
